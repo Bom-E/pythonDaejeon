05e375ad88dce2ba962f1f7eb838ed09a1b229f8
Bom-E <j32575086@gmail.com>
2024-07-12 22:14:46 +0900

    table design finish 240712

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -6,105 +6,112 @@ Base = declarative_base()
 
 # 구현 할 검색 기능들 생각하며 인덱스 짜기.
 
-class CrimeDay(Base):
-    __tablename__ = 'CRIME_DAY'
+# SQLAlchemy의 Index
+# '명시적인 것이 암시적인 것보다 낫다'는 파이썬 철학 반영
+# 1. 모델 정의 시 생성.
+# 2. 데이터베이스에 실제로 인덱스를 만듦.
+# 3. 특정 컬럼에대한 검색, 정렬 등의 작업을 빠르게 만들어줌.
+# 4. 하지만 데이터 삽입, 업데이트, 삭제 시 성능 저하가 발생할 수 있음.
 
-    YEAR = Column(Integer, primary_key=True)
-    CRIME_NAME = Column(String(5), primary_key=True)
-    SUNDAY = Column(Integer)
-    MONDAY = Column(Integer)
-    TUESDAY = Column(Integer)
-    WEDNESDAY = Column(Integer)
-    THURSDAY = Column(Integer)
-    FRIDAY = Column(Integer)
-    SATURDAY = Column(Integer)
+class CrimeDay(Base):
+    __tablename__ = 'crime_day'
+
+    year = Column(Integer, primary_key=True)
+    crime_name = Column(String(5), primary_key=True)
+    sunday = Column(Integer)
+    monday = Column(Integer)
+    tuesday = Column(Integer)
+    wednesday = Column(Integer)
+    thursday = Column(Integer)
+    friday = Column(Integer)
+    saturday = Column(Integer)
 
     __table_args__ = (
-        UniqueConstraint('YEAR', 'CRIME_NAME', name='day_year_crime')
+        UniqueConstraint('year', 'crime_name', name='day_year_crime')
         , Index()
         ,)
 
     def __repr__(self):
-        return f"<CrimeDay(YEAR={self.YEAR}, CRIME_NAME='{self.CRIME_NAME}')>"
+        return f"<CrimeDay(year={self.year}, crime_name='{self.crime_name}')>"
 
 class CrimeTime(Base):
-    __tablename__ = 'CRIME_TIME'
+    __tablename__ = 'crime_time'
 
-    YEAR = Column(Integer, primary_key=True)
-    CRIME_NAME = Column(String(5), primary_key=True)
-    RANGE_00H_TO_04H = Column(Integer)
-    RANGE_04H_TO_07H = Column(Integer)
-    RANGE_07H_TO_12H = Column(Integer)
-    RANGE_12H_TO_18H = Column(Integer)
-    RANGE_18H_TO_20H = Column(Integer)
-    RANGE_20H_TO_24H = Column(Integer)
+    year = Column(Integer, primary_key=True)
+    crime_name = Column(String(5), primary_key=True)
+    range_00h_to_04h = Column(Integer)
+    range_04h_to_07h = Column(Integer)
+    range_07h_to_12h = Column(Integer)
+    range_12h_to_18h = Column(Integer)
+    range_18h_to_20h = Column(Integer)
+    range_20h_to_24h = Column(Integer)
 
     __table_args__ = (
-        UniqueConstraint('YEAR', 'CRIME_NAME', name='time_year_crime')
+        UniqueConstraint('year', 'crime_name', name='time_year_crime')
         ,Index()
         ,)
 
     def __repr__(self):
-        return f"<CrimeTime(YEAR={self.YEAR}, CRIME_NAME='{self.CRIME_NAME}')>"
+        return f"<CrimeTime(year={self.year}, CRIME_NAME='{self.crime_name}')>"
 
 class CrimeFive(Base):
-    __tablename__ = 'CRIME_FIVE'
+    __tablename__ = 'crime_five'
 
-    YEAR = Column(Integer, primary_key=True)
-    POLICE_STATION = Column(String(7), primary_key=True)
-    CATEGORY = Column(String(4), primary_key=True)
-    TOTAL = Column(Integer)
-    MURDER = Column(Integer)
-    ROBBERY = Column(Integer)
-    SEXUAL_ASSAULT = Column(Integer)
-    THEFT = Column(Integer)
-    VIOLENCE = Column(Integer)
+    year = Column(Integer, primary_key=True)
+    police_station = Column(String(7), primary_key=True)
+    category = Column(String(4), primary_key=True)
+    total = Column(Integer)
+    murder = Column(Integer)
+    robbery = Column(Integer)
+    sexual_assault = Column(Integer)
+    theft = Column(Integer)
+    violence = Column(Integer)
 
     __table_args__ = (
-        UniqueConstraint('YEAR', 'POLICE_STATION', 'CATEGORY', name='five_year_police_cate')
-        , Index()
+        UniqueConstraint('year', 'police_station', 'category', name='five_year_police_cate')
         ,)
 
     def __repr__(self):
-        return f"<CrimeFive(YEAR={self.YEAR}, POLICE_STATION='{self.POLICE_STATION}', CATEGORY='{self.CATEGORY}')>"
+        return f"<CrimeFive(year={self.year}, POLICE_STATION='{self.police_station}', CATEGORY='{self.category}')>"
 
 class CrimeCctv(Base):
-    __tablename__ = 'CRIME_CCTV'
+    __tablename__ = 'crime_cctv'
 
-    MANAGE_NO = Column(String(20), primary_key=True)
-    ADDRESS_NAME = Column(String(20))
-    DISTRICT = Column(String(4))
-    DONG = Column(String(10))
-    ADDRESS = Column(String(20))
-    X = Column(Numeric(11, 8))
-    Y = Column(Numeric(10, 8))
+    manage_no = Column(String(20), primary_key=True)
+    address_name = Column(String(20), index=True)
+    district = Column(String(4))
+    dong = Column(String(10))
+    address = Column(String(20), index=True)
+    x = Column(Numeric(11, 8))
+    y = Column(Numeric(10, 8))
 
     __table_args__ = (
-        Index('idx_district_dong', 'DISTRICT', 'DONG')
-        ,)
+        Index('idx_district_dong', 'district', 'dong')
+        , )
 
     def __repr__(self):
-        return f"<CrimeCctv(MANAGE_NO='{self.MANAGE_NO}')>"
+        return f"<CrimeCctv(manage_no='{self.manage_no}')>"
 
 class CrimeHouse(Base):
-    __tablename__ = 'CRIME_HOUSE'
+    __tablename__ = 'crime_house'
 
-    SI_GUN_GU_NAME = Column(String(5), primary_key=True)
-    DONG = Column(String(10))
-    HOUSE_NAME = Column(String(30), primary_key=True)
-    ROAD_ADDR = Column(String(70))
-    LAND_NAME_ADDR = Column(String(70))
-    CALL_NUMBER = Column(String(40))
+    si_gun_gu_name = Column(String(5), primary_key=True)
+    dong = Column(String(10), index=True)
+    house_name = Column(String(30), primary_key=True)
+    road_addr = Column(String(70), index=True)
+    land_name_addr = Column(String(70), index=True)
+    call_number = Column(String(40))
     # CALL_NUMBER 부분은 전화번호 없음 하고 그 NAME에 따라 소비자 보호센터 데이터를 받아 오는 게 더 효울적인지, 아니면 그냥 이렇게 하는 게 효율적인지.
-    POLICE_STATION = Column(String(30))
-    X = Column(Numeric(9, 6))
-    Y = Column(Numeric(8, 6))
-    REG_DATE = Column(DateTime)
+    # 이정도 데이터에서는 그닥 성능 차이 없을 것 같은데??
+    police_station = Column(String(30))
+    x = Column(Numeric(9, 6))
+    y = Column(Numeric(8, 6))
+    reg_date = Column(DateTime)
 
     __table_args__ = (
-        UniqueConstraint('SI_GUN_GU_NAME', 'HOUSE_NAME', name='house_si_gun_gu_house_name')
-        , Index()
+        UniqueConstraint('si_gun_gu_name', 'house_name', name='house_si_gun_gu_house_name')
+        , Index('idx_si_gun_gu_dong', 'si_gun_gu_name', 'dong')
         ,)
 
     def __repr__(self):
-        return f"<CrimeHouse(SI_GUN_GU_NAME='{self.SI_GUN_GU_NAME}', HOUSE_NAME='{self.HOUSE_NAME}')>"
+        return f"<CrimeHouse(si_gun_gu_name='{self.si_gun_gu_name}', HOUSE_NAME='{self.house_name}')>"
